c1622a50fba8d83c211d04dbd572956dfdb6d0ab
glory <jhardcy08@gmail.com>
2020-09-25 23:00:42 +0800

    setup babelrc and webpack.config.js for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,47 @@
+var path = require('path');
+var HtmlWebpackPlugin =  require('html-webpack-plugin');
 
+module.exports = {
+    entry : {
+        main: './src/js/index.js',
+        vendor:
+        [
+            './src/js/vendors/vendor1.js', 
+            './src/js/vendors/vendor2.js'
+        ]
+    },
+    output : {
+        path : path.resolve(__dirname , 'dist'),
+        filename: '[contentHash].js'
+    },
+    module : {
+        rules : [
+            {test : /\.(js)$/, use:{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-class-properties']
+                }
+            }},
+            {test : /\.css$/, use:['style-loader', 'css-loader']},
+            {test: /\.(png|jpg|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    plugins : [
+        new HtmlWebpackPlugin ({
+            template : './public/index.html',
+          
+        })
+    ]
+}
